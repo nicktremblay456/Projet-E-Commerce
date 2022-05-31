5f177cff7e89dcabdf19ac9f9c44ac282a308610
Nicolas Tremblay <nick_tremblay456@hotmail.com>
2022-05-31 14:46:42 -0400

    changed a href in accueil

--- public/accueil.php
+++ public/accueil.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil</title>
 
+    <? require('index.php'); ?>
+
     <link rel="stylesheet" href="../public/css/style.css">
 </head>
 <header class="accueil_header">
