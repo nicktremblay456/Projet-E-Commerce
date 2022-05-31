909a2a7f7b0ed315fcab6e8ecf9fa7504d42faf0
Janick Bergeron <janick.bergeron392@outlook.fr>
2022-05-31 16:20:27 -0400

    fork fix

---
+++ public/magasin.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../public/css/style.css">
+    <link rel="stylesheet" href="../public/css/bootstrap.css">
+    <title>Document</title>
+    <? require('index.php') ?>
+</head>
+<header class="accueil_header">
+    <?= $header ?>
+</header>
+
+<body>
+    <div class="search-bar">
+
+        <input type="text" placeholder="Recherche..">
+
+    </div>
+    <div class="magasin-content">
+        <div class="sidebar">
+            <label class="filters">One
+                <input type="radio" name="radio">
+                <span class="checkmark"></span>
+            </label>
+    
+            <label class="filters">Two
+                <input type="radio" name="radio">
+                <span class="checkmark"></span>
+            </label>
+    
+            <label class="filters">Three
+                <input type="radio" name="radio">
+                <span class="checkmark"></span>
+            </label>
+    
+            <label class="filters">Four
+                <input type="radio" name="radio">
+                <span class="checkmark"></span>
+            </label>
+        </div>
+        <div class="container-content">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                    <div class="card" style="width: 18rem;">
+                            <img src="../src/placeholder.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Add to Cart</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                    <div class="card" style="width: 18rem;">
+                            <img src="../src/placeholder.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Add to Cart</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                    <div class="card" style="width: 18rem;">
+                            <img src="../src/placeholder.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Add to Cart</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                    <div class="card" style="width: 18rem;">
+                            <img src="../src/placeholder.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Add to Cart</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                    <div class="card" style="width: 18rem;">
+                            <img src="../src/placeholder.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Add to Cart</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                    <div class="card" style="width: 18rem;">
+                            <img src="../src/placeholder.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Add to Cart</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                    <div class="card" style="width: 18rem;">
+                            <img src="../src/placeholder.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Add to Cart</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                    <div class="card" style="width: 18rem;">
+                            <img src="../src/placeholder.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Add to Cart</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
