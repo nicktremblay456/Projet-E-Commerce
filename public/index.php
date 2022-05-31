4c3495246a5ad29231448ccba8a570636f416a75
Nicolas Tremblay <nick_tremblay456@hotmail.com>
2022-05-31 13:53:16 -0400

    added accueil

--- public/index.php
+++ public/index.php
@@ -5,7 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link rel="stylesheet" href="../public/css/style.css">
 </head>
+<header class="accueil_header">
+    <h1>Hello</h1>
+</header>
 <body>
     
 </body>
