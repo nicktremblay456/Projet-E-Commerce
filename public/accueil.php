3a6a2af108149277c21121f1d368ac64062e808f
Nicolas Tremblay <nick_tremblay456@hotmail.com>
2022-06-01 09:30:29 -0400

    moved header in body

--- public/accueil.php
+++ public/accueil.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
     <title>Accueil</title>
 
     <link rel="stylesheet" href="../public/css/style.css">
@@ -12,10 +12,9 @@
 
     <? require('index.php'); ?>
 </head>
-<header class="accueil_header">
-    <?= $header ?>
-</header>
 <body>
+    <?= $header ?>
+    
     <?= $accueilBody ?>
 
     <?= $footer ?>
--- public/header.php
+++ public/header.php
@@ -1,9 +1,11 @@
-<ul>
-    <li><a href="accueil.php">Accueil</a></li>
-    <li><a href="magasin.php">Magasin</a></li>
-    <li><a href="contacte.php">Contacte</a></li>
-
-    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-        <button class="btn btn-primary me-md-2 signin-button" type="button">Sign in</button>
-    </div>
-</ul>
+<header class="accueil_header">
+    <ul>
+        <li><a href="accueil.php">Accueil</a></li>
+        <li><a href="magasin.php">Magasin</a></li>
+        <li><a href="contacte.php">Contacte</a></li>
+    
+        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+            <button class="btn btn-primary me-md-2 signin-button" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">Sign in</button>
+        </div>
+    </ul>
+</header>
