b68fec315f71fe29b38d4fe81c16f59cfbb68baf
Nicolas Tremblay <nick_tremblay456@hotmail.com>
2022-06-01 10:06:57 -0400

    changed header css class

--- public/header.php
+++ public/header.php
@@ -1,5 +1,5 @@
 <header class="accueil_header">
-    <ul>
+    <ul class="header">
         <li><a href="accueil.php">Accueil</a></li>
         <li><a href="magasin.php">Magasin</a></li>
         <li><a href="contacte.php">Contacte</a></li>
