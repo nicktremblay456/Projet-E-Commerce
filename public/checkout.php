efdb42e3e0fed1a1becdf429f2f50c1d06d87c62
Nicolas Tremblay <nick_tremblay456@hotmail.com>
2022-06-03 08:34:14 -0400

    put the checkout page in french

--- public/checkout.php
+++ public/checkout.php
@@ -26,42 +26,42 @@
                 <form action="/action_page.php">
                     <div style="display: flex; flex-direction: row; ">
                         <div style="margin-right: 50px;">
-                            <h3>Billing Address</h3>
-                            <label for="fname"><i class="fa fa-user"></i> Full Name</label>
+                            <h3>Adresse de facturation</h3>
+                            <label for="fname"><i class="fa fa-user"></i> Prénom et Nom</label>
                             <input type="text" id="fname" name="firstname" placeholder="John M. Doe">
                             <label for="email"><i class="fa fa-envelope"></i> Email</label>
                             <input type="text" id="email" name="email" placeholder="john@example.com">
-                            <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
+                            <label for="adr"><i class="fa fa-address-card-o"></i> Adresse</label>
                             <input type="text" id="adr" name="address" placeholder="542 W. 15th Street">
-                            <label for="city"><i class="fa fa-institution"></i> City</label>
+                            <label for="city"><i class="fa fa-institution"></i> Ville</label>
                             <input type="text" id="city" name="city" placeholder="New York">
 
                             <div class="row">
                                 <div class="col-50">
-                                    <label for="state">State</label>
+                                    <label for="state">État</label>
                                     <input type="text" id="state" name="state" placeholder="NY">
                                 </div>
                                 <div class="col-50">
-                                    <label for="zip">Zip</label>
+                                    <label for="zip"> Code Postal</label>
                                     <input type="text" id="zip" name="zip" placeholder="10001">
                                 </div>
                             </div>
                         </div>
 
                         <div>
-                            <h3>Payment</h3>
-                            <label for="fname">Accepted Cards</label>
+                            <h3>Paiement</h3>
+                            <label for="fname">Cartes acceptées</label>
                             <div class="icon-container">
                                 <i class="fa fa-cc-visa" style="color:navy;"></i>
                                 <i class="fa fa-cc-amex" style="color:blue;"></i>
                                 <i class="fa fa-cc-mastercard" style="color:red;"></i>
                                 <i class="fa fa-cc-discover" style="color:orange;"></i>
                             </div>
-                            <label for="cname">Name on Card</label>
+                            <label for="cname">Nom sur la carte</label>
                             <input type="text" id="cname" name="cardname" placeholder="John More Doe">
-                            <label for="ccnum">Credit card number</label>
+                            <label for="ccnum">Numéro de Carte de Crédit</label>
                             <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444">
-                            <label for="expmonth">Exp Month</label>
+                            <label for="expmonth">Mois d'expiration</label>
                             <input type="text" id="expmonth" name="expmonth" placeholder="September">
                             <div class="row">
                                 <div class="col-50">
@@ -77,7 +77,7 @@
 
                     </div>
                     <label>
-                        <input type="checkbox" checked="checked" name="sameadr"> Shipping address same as billing
+                        <input type="checkbox" checked="checked" name="sameadr"> Adresse de livraison identique à celle de facturation
                     </label>
                     <input type="submit" class="button-modal" style="width: 200px;" value="Continue to checkout" class="btn">
                 </form>
@@ -85,12 +85,12 @@
         </div>
         <div class="card">
             <div style="background-color: white;">
-                <h4>Cart <span style="color:black"><i class="fa fa-shopping-cart"></i> <b>4</b></span></h4>
+                <h4>Panier <span style="color:black"><i class="fa fa-shopping-cart"></i> <b>4</b></span></h4>
                 <? for ($i = 0; $i < 4; $i++) { ?>
-                    <p><a href="#">Product Name</a> <span class="price">$15</span></p>
+                    <p><a href="#">Nom du produit</a> <span class="price">$15</span></p>
                 <? } ?>
                 <hr>
-                <p>Total <span class="price" style="color:black"><b>$30</b></span></p>
+                <p>Totale <span class="price" style="color:black"><b>$30</b></span></p>
             </div>
         </div>
     </div>
