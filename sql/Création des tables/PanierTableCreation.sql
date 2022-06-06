1a5ef63489b0381c34e6030a72e1455ed41f5f75
Janick Bergeron <janick.bergeron392@outlook.fr>
2022-06-06 16:25:27 -0400

    panier script fix

--- sql/Création des tables/PanierTableCreation.sql
+++ sql/Création des tables/PanierTableCreation.sql
@@ -4,6 +4,6 @@ Clientid int not null,
 Itemid int not null,
 Quantity int not null,
 primary key(ID),
-foreign key(Clientid) references client(Clientid),
+foreign key(Clientid) references client(ID),
 foreign key(Itemid) references produit(ID)
 );
