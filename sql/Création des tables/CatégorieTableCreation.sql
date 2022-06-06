4328baa4d7523bf00bc5fdb9ab5bb90ed8075349
Nicolas Tremblay <nick_tremblay456@hotmail.com>
2022-06-06 16:16:40 -0400

    fixed all sql scripts

--- sql/Création des tables/CatégorieTableCreation.sql
+++ sql/Création des tables/CatégorieTableCreation.sql
@@ -1,6 +1,4 @@
 CREATE TABLE categorie (
     ID int NOT NULL AUTO_INCREMENT Primary key,
     Categorie varchar(255) NOT NULL
-
-    foreign key(ID) references produit(ID);
 );
--- sql/Création des tables/ProduitTableCreation.sql
+++ sql/Création des tables/ProduitTableCreation.sql
@@ -1,12 +1,11 @@
 CREATE TABLE produit (
     ID int AUTO_INCREMENT PRIMARY KEY,
     Name varchar(255) NOT NULL,
-    Category int NOT NULL,
     Description varchar(255) NOT NULL,
     Price float NOT NULL,
     CurrentStock int NOT NULL,
+    Category int NOT NULL,
     Path varchar(255) NOT NULL,
 
-    foreign key(Categorie) references categorie(ID)
+    foreign key(Category) references categorie(ID)
 );
-
