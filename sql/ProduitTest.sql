7f81d3896c65f537cbb506b4d4fd1735b30a6eee
Nicolas Tremblay <nick_tremblay456@hotmail.com>
2022-06-06 12:42:47 -0400

    fix sql script

--- sql/ProduitTest.sql
+++ sql/ProduitTest.sql
@@ -1,8 +1,5 @@
 INSERT INTO Produit (Name, Category, Description, Price, CurrentStock)
-VALUES (Aspiro-1000, Aspirateur, aspirateur, 99.99, 10);
-INSERT INTO Produit (Name, Category, Description, Price, CurrentStock)
-VALUES (Aspiro-2000, Aspirateur, aspirateur, 199.99, 10);
-INSERT INTO Produit (Name, Category, Description, Price, CurrentStock)
-VALUES (Aspiro-3000, Aspirateur, aspirateur, 299.99, 10);
-INSERT INTO Produit (Name, Category, Description, Price, CurrentStock)
-VALUES (Aspiro-4000, Aspirateur, aspirateur, 399.99, 10);
+VALUES ("Aspiro-1000", "Aspirateur", "aspirateur", 99.99, 10),
+("Aspiro-2000", "Aspirateur", "aspirateur", 199.99, 10),
+("Aspiro-3000", "Aspirateur", "aspirateur", 299.99, 10),
+("Aspiro-4000", "Aspirateur", "aspirateur", 399.99, 10);
