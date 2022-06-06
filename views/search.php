fdead538e66c4d943c9360951a5152b66ea0c5a4
Nicolas Tremblay <nick_tremblay456@hotmail.com>
2022-06-06 13:52:07 -0400

    moved php code from search.php to search_controller.php

--- views/search.php
+++ views/search.php
@@ -1,14 +1,3 @@
-<?php
-
-require $ROOT_PATH . 'views/db_controller.php';
-
-
-if (isset($_POST['search'])) {
-    $search = "%{$_POST['search']}%";
-    $products = sqlQuery("SELECT * FROM produit WHERE Description LIKE '$search'")->fetchAll();
-}
-?>
-
 <!DOCTYPE html>
 <html lang="fr">
 
