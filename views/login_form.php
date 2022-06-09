17e571e01495cd2cf363564f767967b730e7a107
Nicolas Tremblay <nick_tremblay456@hotmail.com>
2022-06-09 10:46:58 -0400

    reworked login_form.php

--- views/login_form.php
+++ views/login_form.php
@@ -1,37 +1,7 @@
-<!DOCTYPE html>
-<html lang="fr">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title><?= $title ?></title>
-
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/bootstrap.css">
-</head>
-
-<body>
-    <?= $header ?>
-    
-    <? if (isset($_SESSION['username']) && isset($_SESSION['psw'])) { ?>
-        <div class="card" style="width: 50%; margin: auto; margin-top: 10px;">
-            <div class="card-body" style="text-align: center;">
-                <h5 class="card-title">Bienvenue <?= $username ?></h5>
-            </div>
-        </div>
-    <? } else { ?>
-        <?= $loginModal ?>
-        <?= $signupModal ?>
-        <div class="card" style="width: 50%; margin: auto; margin-top: 10px;">
-            <div class="card-body">
-                <h5 class="card-title">Nom d'utilisateur ou mot de passe invalid...</h5>
-            </div>
-        </div>
-    <? } ?>
-
-    <?= $footer ?>
-</body>
-
-</html>
+if (isset($_SESSION['username']) && isset($_SESSION['psw'])) {
+    header('location: /');
+} else {
+    echo '<script>alert("Nom et/ou mot de passe invalide..."); location="/";</script>';
+}
