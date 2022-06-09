92046bad808b9abfa6f79faa1bcc7e0ca50339e5
Nicolas Tremblay <nick_tremblay456@hotmail.com>
2022-06-09 11:47:30 -0400

    removed useless onsubmit event in signup modal

--- views/signup_modal.php
+++ views/signup_modal.php
@@ -24,7 +24,7 @@
             <!--<p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>-->
 
             <div class="container clearfix" style="background-color:#f1f1f1">
-                <button type="submit" onsubmit="onSubmit(event)" id="sub" class="button-modal">Sign Up</button>
+                <button type="submit" id="sub" class="button-modal">Sign Up</button>
                 <button type="button" class="button-modal" onclick="document.getElementById('id02').style.display='none'" class="cancelbtn">Cancel</button>
             </div>
         </div>
