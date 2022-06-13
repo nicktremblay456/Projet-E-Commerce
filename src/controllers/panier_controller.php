8e5fd5c566b8d2ea585327f31f1a775f93e024fc
Nicolas Tremblay <nick_tremblay456@hotmail.com>
2022-06-13 08:40:56 -0400

    added safe check in panier_controller

--- src/controllers/panier_controller.php
+++ src/controllers/panier_controller.php
@@ -1,8 +1,10 @@
 <?php
 require_once $ROOT_PATH . 'views/db_controller.php';
 
-$userId = $_COOKIE['LOGGED_USER'];
-$itemId = $_POST['itemid'];
-
-sqlQueryPrepare("INSERT INTO panier VALUES(null, :ClientId, :ItemId, :Quantity);",
-                ['ClientId' => $userId, 'ItemId' => $itemId, 'Quantity' => 1]);
+if (isset($_COOKIE['LOGGED_USER'])) {
+    $userId = $_COOKIE['LOGGED_USER'];
+    $itemId = $_POST['itemid'];
+    
+    sqlQueryPrepare("INSERT INTO panier VALUES(null, :ClientId, :ItemId, :Quantity);",
+                    ['ClientId' => $userId, 'ItemId' => $itemId, 'Quantity' => 1]);
+}
