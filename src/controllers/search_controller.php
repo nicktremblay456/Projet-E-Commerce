d41a05485da0220ba86dcaf778b2dec2c5d3eb3b
Nicolas Tremblay <nick_tremblay456@hotmail.com>
2022-06-17 12:01:25 -0400

    bug fixed in deletecart_controller

--- src/controllers/search_controller.php
+++ src/controllers/search_controller.php
@@ -1,8 +1,10 @@
 <?php
 require_once $ROOT_PATH . 'src/controllers//db_controller.php';
 
+$products = [];
 
 if (isset($_POST['search'])) {
+    global $products;
     $search = "%{$_POST['search']}%";
     $products = sqlQuery("SELECT * FROM produit WHERE Description LIKE '$search'")->fetchAll();
 }
