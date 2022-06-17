ff3f2d55bf86a5ab295c2c0ae1a3e258b12d85f1
Janick Bergeron <janick.bergeron392@outlook.fr>
2022-06-17 11:49:46 -0400

    minor fix to admin panel

--- src/controllers/access_mgmt_controller.php
+++ src/controllers/access_mgmt_controller.php
@@ -1,17 +1,19 @@
 <?php
 
 require $ROOT_PATH . 'src/controllers/db_controller.php';
+require_once $ROOT_PATH . 'src/controllers/control_panel_controller.php';
 
 generateViews();
 
-$userToChange = $_POST['userID'];
-$permission = $_POST['userAccess'];
+if (isset($_POST['userID'],$_POST['userAccess'])){
 
-sqlQueryPrepare("UPDATE client SET isAdmin = :permission WHERE ID = :userID",
-["permission" => $permission, "userID" => $userToChange]);
-
-$result = sqlQuery("SELECT ID, Name, Category FROM produit;");
-$usersAccount = sqlQuery("SELECT ID, UserName, isAdmin FROM client;"); 
+    $userToChange = $_POST['userID'];
+    $permission = $_POST['userAccess'];
+    
+    sqlQueryPrepare("UPDATE client SET isAdmin = :permission WHERE ID = :userID",
+    ["permission" => $permission, "userID" => $userToChange]);
+    
+}
 
 
 require $ROOT_PATH . "views/control_panel.php";
