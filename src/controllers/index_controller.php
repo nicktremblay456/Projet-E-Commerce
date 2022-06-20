1b81dc9818ae64cfded1b24831807c90c5d8ffc3
Nicolas Tremblay <nick_tremblay456@hotmail.com>
2022-06-20 13:26:19 -0400

    handle current stock in magasin and accueil

--- src/controllers/index_controller.php
+++ src/controllers/index_controller.php
@@ -1,9 +1,17 @@
 <?php
 require_once $ROOT_PATH . 'src/controllers/db_controller.php';
+require_once $ROOT_PATH . 'src/controllers/auth_controller.php';
 
-$videoGames = sqlQuery("SELECT * FROM produit WHERE Category = 3")->fetchAll();
-$sports = sqlQuery("SELECT * FROM produit WHERE Category = 7")->fetchAll();
-$clothes = sqlQuery("SELECT * FROM produit WHERE Category = 8")->fetchAll();
+if (is_user_login()) {
+    $userId = $_SESSION['userId'];
+    $videoGames = sqlQueryPrepare("SELECT produit.*, produit.CurrentStock, panier.Quantity, ifnull(CurrentStock - Quantity, CurrentStock) as Diff FROM produit LEFT JOIN (SELECT * FROM panier WHERE Clientid = :userId) as panier ON produit.ID = panier.Itemid WHERE Category = 3;", ['userId' => $userId]);
+    $sports = sqlQueryPrepare("SELECT produit.*, produit.CurrentStock, panier.Quantity, ifnull(CurrentStock - Quantity, CurrentStock) as Diff FROM produit LEFT JOIN (SELECT * FROM panier WHERE Clientid = :userId) as panier ON produit.ID = panier.Itemid WHERE Category = 7;", ['userId' => $userId]);
+    $clothes = sqlQueryPrepare("SELECT produit.*, produit.CurrentStock, panier.Quantity, ifnull(CurrentStock - Quantity, CurrentStock) as Diff FROM produit LEFT JOIN (SELECT * FROM panier WHERE Clientid = :userId) as panier ON produit.ID = panier.Itemid WHERE Category = 8;", ['userId' => $userId]);
+} else {
+    $videoGames = sqlQuery("SELECT * FROM produit WHERE Category = 3")->fetchAll();
+    $sports = sqlQuery("SELECT * FROM produit WHERE Category = 7")->fetchAll();
+    $clothes = sqlQuery("SELECT * FROM produit WHERE Category = 8")->fetchAll();
+}
 
 generateViews();
 
