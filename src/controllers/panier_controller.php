98f82944a67bd0e1ba0c39d76f208b53fd943559
Nicolas Tremblay <nick_tremblay456@hotmail.com>
2022-06-20 08:45:03 -0400

    added quantity check in panier_controller

--- src/controllers/panier_controller.php
+++ src/controllers/panier_controller.php
@@ -2,12 +2,18 @@
 require_once $ROOT_PATH . 'src/controllers//db_controller.php';
 
 if (get_cookie('LOGGED_USER')) {
-    $userId = $_SESSION['userId'];
-    $itemId = $_POST['itemid'];
-    $quantity = $_POST['quantity'];
+    if (isset($_POST['itemid'], $_POST['quantity'], $_SESSION['userId'])) {
+        $userId = $_SESSION['userId'];
+        $itemId = $_POST['itemid'];
+        $quantity = $_POST['quantity'];
 
-    # add to card if item exist
-    if (!sqlQueryPrepare("INSERT INTO panier VALUES(null, :ClientId, :ItemId, :Quantity);", ['ClientId' => $userId, 'ItemId' => $itemId, 'Quantity' => $quantity]) === false) {
-        echo '<script>alert("Produit invalide..."); location="/";</script>';
+        if ($quantity <= 0) {
+            $quantity = 1;
+        }
+
+        # add to card if item exist
+        if (!sqlQueryPrepare("INSERT INTO panier VALUES(null, :ClientId, :ItemId, :Quantity);", ['ClientId' => $userId, 'ItemId' => $itemId, 'Quantity' => $quantity]) === false) {
+            echo '<script>alert("Produit invalide..."); location="/";</script>';
+        }
     }
 }
